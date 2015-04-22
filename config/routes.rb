3d40c1d98a62185b3427b02046ae53d2da7532cb
roccofarrell <farrell.roc@gmail.com>
2015-04-22 14:24:48 +0000

    Fixed home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'event_table/home'
 
-  root 'event_table/home'
+  root 'event_table#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
