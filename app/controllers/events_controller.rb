998671965bd9f235db095ca2f3923dffd940128d
roccofarrell <farrell.roc@gmail.com>
2015-04-22 17:47:21 +0000

    Working version of event display is up. Still need to fix overlapping events, exact time ranges (off by 15 minutes), multiple days

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,13 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.find(1)
-  end
+    @events = Event.all
     
+    @day_time_reference = DateTime.new(2015, 12, 8, 7, 0 ,0)
+    @day_time_exact = DateTime.new()
+    
+  end
+  
+  def new
+    @event = Event.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'event_table/home'
-
-  root 'event_table#home'
+  root 'events#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
